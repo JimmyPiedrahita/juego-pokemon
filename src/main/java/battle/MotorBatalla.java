32f52d2de660abbb33548248b03b4910f9e05dc4
Jimmy Piedrahita <jimmy22piedrahita@gmail.com>
2026-05-14 12:17:36 -0500

    fix: corregir la lógica de ejecución de turnos en MotorBatalla para manejar correctamente las prioridades de los comandos

--- src/main/java/battle/MotorBatalla.java
+++ src/main/java/battle/MotorBatalla.java
@@ -1,18 +1,29 @@
 package battle;
 
 import battle.command.ComandoTurno;
+import java.util.Random;
 
 public class MotorBatalla {
+
+    private Random random = new Random();
 
     public void ejecutarTurno(ComandoTurno cmdJugador, ComandoTurno cmdRival) {
         core.events.GameEventManager.getInstance().notifyMessage("\n[RES]");
         
-        if (cmdJugador.getPrioridad() >= cmdRival.getPrioridad()) {
+        if (cmdJugador.getPrioridad() > cmdRival.getPrioridad()) {
             cmdJugador.ejecutar();
             cmdRival.ejecutar();
+        } else if (cmdJugador.getPrioridad() < cmdRival.getPrioridad()) {
+            cmdRival.ejecutar();
+            cmdJugador.ejecutar();
         } else {
-            cmdRival.ejecutar();
-            cmdJugador.ejecutar();
+            if (random.nextBoolean()) {
+                cmdJugador.ejecutar();
+                cmdRival.ejecutar();
+            } else {
+                cmdRival.ejecutar();
+                cmdJugador.ejecutar();
+            }
         }
     }
 }
